3c6754b67b8e17d37763a894157b4a3390c76af6
Joao <joaopccosta@gmail.com>
2019-07-26 21:19:08 +0100

    server routes added. add via curl working by POST. Jinja2 dependency added for later templating UI

--- server.py
+++ server.py
@@ -1,1 +1,31 @@
-print("This is a server")
+from flask import Flask, request, render_template, abort
+from src.GitCLI import GitCLI
+from src.Project import Project
+
+app = Flask(__name__)
+gitCLI = GitCLI()
+projects = {}
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/add/<name>", methods=['POST'])
+def addProject(name):
+    url = request.get_data().decode("utf-8")
+    print(f"Fetching {url}")
+    project = Project(gitCLI, name, url)
+    project.fetchCommits()
+    projects[name] = project
+    return "200"
+
+@app.route("/list/<name>")
+def listProject(name):
+    return "200"
+
+
+@app.route("/json/<name>")
+def listProjectAsJson(name):
+    return "200"
+
+if __name__ == "__main__":
+    app.run()
