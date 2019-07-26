30e934f28394c034cf2b05ea295baba274dd5ebd
Joao <joaopccosta@gmail.com>
2019-07-26 17:44:50 +0100

    CodacyCLI small refactor

--- src/CodacyCLI.py
+++ src/CodacyCLI.py
@@ -5,14 +5,19 @@ from src.Constants import *
 
 class CodacyCLI:
     def fetchCommits(self, url):
-        name = str(url).split("/")[1].split(".")[0]
+        name = self.getProjectNameFromURL(url)
         subprocess.run(GIT_CLONE_COMMMAND+[url])
         output = subprocess.run (GIT_LOG_COMMAND, stdout=subprocess.PIPE, cwd=f"./{name}")
-        contents = str(output.stdout.decode(UTF_ENCONDING)).replace("\"", "").split("\n")
-        print(f"{contents}")
+        gitLogRestuls = str(output.stdout.decode(UTF_ENCONDING)).replace("\"", "").split("\n")
         subprocess.run(RM_FOLDER_COMMAND+[name])
 
-        return contents
+        print(f"{gitLogRestuls}")
+        return gitLogRestuls
+
+    def getProjectNameFromURL(self, url):
+        name = str(url).split("/")[1].split(".")[0]
+        return name
+
 
 if __name__ == '__main__':
     codacyCLI = CodacyCLI()
