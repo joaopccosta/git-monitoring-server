83db22d3e4d4f889c47e8033caa8eba9e4a28122
Joao <joaopccosta@gmail.com>
2019-07-27 21:38:03 +0100

    refactor dashboard template and main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,32 +1,5 @@
-variable "dockerhost_address" {
-  default = "127.0.0.1"
-}
-
-variable "dockerhost_port" {
-  default = 2375
-}
-variable "serverport" {
-  default = 5000
-}
-
-variable "prometheusport" {
-  default = 9090
-}
-
-variable "grafanaport" {
-  default = 3000
-}
-
 provider "docker" {
   host = "tcp://${var.dockerhost_address}:${var.dockerhost_port}"
-}
-
-data "local_file" "prometheus-yml" {
-    filename = abspath("${path.module}/prometheus/prometheus.yml")
-}
-
-data "local_file" "prometheus-dashboard-1" {
-    filename = abspath("${path.module}/prometheus/prometheus-dashboard-template1.json")
 }
 
 resource "docker_container" "prometheus" {
@@ -60,33 +33,6 @@ resource "docker_container" "webserver" {
   }
 }
 
-resource "docker_image" "prometheus" {
-  name = "prom/prometheus:latest"
-}
-
-resource "docker_image" "grafana" {
-  name = "grafana/grafana:latest"
-}
-
-resource "docker_image" "server" {
-  name = "server:latest"
-  keep_locally = true
-}
-
-
-output "grafana_address" {
-  value = "${docker_container.grafana.*.ip_address}"
-}
-
-
-output "prometheus_address" {
-  value = "${docker_container.prometheus.*.ip_address}"
-}
-
-output "webserver_address" {
-  value = "${docker_container.webserver.*.ip_address}"
-}
-
 provider "grafana" {
   url = "http://${docker_container.grafana.ip_address}:${var.grafanaport}"
   auth = "admin:admin"
@@ -99,6 +45,5 @@ resource "grafana_data_source" "prometheus" {
 }
 
 resource "grafana_dashboard" "metrics1" {
-//  config_json = replace(data.local_file.prometheus-dashboard-1.content, "server-ip", docker_container.webserver.ip_address)
   config_json = data.local_file.prometheus-dashboard-1.content
 }
