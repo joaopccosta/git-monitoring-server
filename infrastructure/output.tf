d1ddb76fd29435f14440af366acff88acf43fb26
Joao <joaopccosta@gmail.com>
2019-07-27 22:02:31 +0100

    exposing ports as outputs

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,11 +1,22 @@
 output "grafana_address" {
   value = "${docker_container.grafana.*.ip_address}"
 }
+output "grafana_port" {
+  value = "${docker_container.grafana.*.ports}"
+}
 
 output "prometheus_address" {
   value = "${docker_container.prometheus.*.ip_address}"
 }
 
+output "prometheus_port" {
+  value = "${docker_container.prometheus.*.ports}"
+}
+
 output "webserver_address" {
   value = "${docker_container.webserver.*.ip_address}"
+}
+
+output "webserver_port" {
+  value = "${docker_container.webserver.*.ports}"
 }
